c88fd5d7d48961568ee939e848e141d5472ddb62
vinnakotasaishanmukh <saishanmukh.2006@gmail.com>
2025-09-23 23:22:48 +0530

    changes made

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 const DEPLOYED='https://e-commerce-server-production-0873.up.railway.app'
-const LOCALHOST='http://localhost:5454'
+const LOCALHOST='http://localhost:3030'
 
 export const API_BASE_URL = LOCALHOST
 
